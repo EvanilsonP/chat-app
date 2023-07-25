cc7844645ea115e97c1fb82fa11722a7b115d193
EvanilsonP <pevanilson99@gmail.com>
2023-07-25 10:46:23 -0300

    Creating index.js page

---
+++ index.js
@@ -0,0 +1,16 @@
+// Salvar dados no local storage quando for logar/acessar a página do chat
+const setLocalStorage = (usuario) => localStorage.setItem("usuario", JSON.stringify(usuario));
+
+function btnEntrar() {
+    const inputNome = document.getElementById('nome').value;
+    const IdUsuario = (Math.random() * 1000).toString();
+
+    // Salvando o nome e ID do usuário
+    setLocalStorage({
+        nome: inputNome,
+        meuId: IdUsuario
+    });
+
+    // Direcionar para a página do chat passando os parametros na URL
+    window.location.href="chat.html?usuarionome=" + inputNome + "&meuId=" + IdUsuario;
+};
